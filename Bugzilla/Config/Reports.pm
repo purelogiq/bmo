c10dc8a50de4fd815855ce6aa7ac4425cba81efd
Dylan William Hardison <dylan@hardison.net>
2018-09-04 21:55:09 -0400

    Add configuration to admin params

---
+++ Bugzilla/Config/Reports.pm
@@ -0,0 +1,32 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# This Source Code Form is "Incompatible With Secondary Licenses", as
+# defined by the Mozilla Public License, v. 2.0.
+
+package Bugzilla::Config::Reports;
+
+use 5.10.1;
+use strict;
+use warnings;
+
+use Bugzilla::Config::Common;
+
+our $sortkey = 1100;
+
+sub get_param_list {
+    my $class      = shift;
+    my @param_list = (
+        {
+            name    => 'report_secbugs_emails',
+            type    => 't',
+            default => ''
+        },
+        {
+            name    => 'report_secbugs_products',
+            type    => 'l',
+            default => ''
+        },
+     );
+}
